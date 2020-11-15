873a83f5586abcad6232493c6ed460f22cea2d1d
Đinh Xuân Hùng <xuanhung312000@gmail.com>
2020-11-15 12:03:15 +0700

    Update

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,370 +3,370 @@ const { DataTypes } = require("sequelize");
 
 //Sequelize
 const sequelize = new Sequelize("trode", process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  dialect: 'mysql'
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql'
 });
 
 async function sequelizeInit() {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync({force: true});
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ force: true });
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 };
 
 const Nguoi_dung = sequelize.define('nguoi_dung', {
-  // Model attributes are defined here
-  id_nguoi_dung: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  ten_nguoi_dung: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    validate: {
-      isEmail: {
-        msg: "Please enter a true email"
-      }
+    // Model attributes are defined here
+    id_nguoi_dung: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
-    unique: true,
-    allowNull: false
-  },
-  sdt: {
-    type: DataTypes.INTEGER,
-    allowNull: true
-  },
-  mat_khau: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  dia_chi: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  ho_va_ten: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  gioi_tinh: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  ngay_sinh: {
-    type: DataTypes.DATE,
-    allowNull: true
-  }
+    ten_nguoi_dung: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        validate: {
+            isEmail: {
+                msg: "Please enter a true email"
+            }
+        },
+        unique: true,
+        allowNull: false
+    },
+    sdt: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    mat_khau: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dia_chi: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    ho_va_ten: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    gioi_tinh: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    ngay_sinh: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
 }, {
-  freezeTableName: true
-  // Other model options go here
+    freezeTableName: true
+        // Other model options go here
 });
 
 const Phong_tro = sequelize.define('phong_tro', {
-  // Model attributes are defined here
-  id_phong_tro: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  thanh_pho: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  quan_huyen: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phuong_xa: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  dia_chi_cu_the: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  dien_tich: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  phong_ngu: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  phong_tam: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  phong_khach: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phong_bep: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  thong_tin_khac: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  nguoi_lon: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  tre_em: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  tre_nho: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  tong_so_nguoi: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  gia_phong: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  phan_loai: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  ten_phong: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  hinh_thuc_thue: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phan_loai_khac: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  khong_lam_on: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  khong_hut_thuoc: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  khong_thu_cung: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  quy_dinh_khac: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  path_anh_noi_bat : {
-    type : DataTypes.STRING,
-    allowNull : true
-  }
+    // Model attributes are defined here
+    id_phong_tro: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    thanh_pho: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    quan_huyen: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phuong_xa: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dia_chi_cu_the: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dien_tich: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    phong_ngu: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    phong_tam: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    phong_khach: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phong_bep: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    thong_tin_khac: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nguoi_lon: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    tre_em: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    tre_nho: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    tong_so_nguoi: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    gia_phong: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    phan_loai: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    ten_phong: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    hinh_thuc_thue: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phan_loai_khac: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    khong_lam_on: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    khong_hut_thuoc: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    khong_thu_cung: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    quy_dinh_khac: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    path_anh_noi_bat: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
 }, {
-  freezeTableName: true
-  // Other model options go here
+    freezeTableName: true
+        // Other model options go here
 });
 
 const Tien_ich = sequelize.define('tien_ich_tien_nghi', {
-  id_phong_tro: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-  },
-  wifi: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  internet: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  dieu_hoa: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  tv: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  quat: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  nong_lanh: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  may_giat: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  tu_lanh: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  gia_phoi_quan_ao: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  cua_so: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  ban_cong: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  tu_quan_ao: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  coi_bao_chay: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  binh_chua_chay: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  thang_may: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  thang_bo: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  cho_do_rac: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  cho_de_xe: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-},{
-  freezeTableName: true
+    id_phong_tro: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+    },
+    wifi: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    internet: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    dieu_hoa: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    tv: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    quat: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    nong_lanh: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    may_giat: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    tu_lanh: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    gia_phoi_quan_ao: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    cua_so: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    ban_cong: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    tu_quan_ao: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    coi_bao_chay: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    binh_chua_chay: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    thang_may: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    thang_bo: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    cho_do_rac: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    cho_de_xe: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+}, {
+    freezeTableName: true
 });
 
 const Hinh_anh = sequelize.define('hinh_anh', {
-  // Model attributes are defined here
-  id_anh: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-  },
-  path_anh: {
-    type: DataTypes.STRING(1000),
-    allowNull: false
-  }
+    // Model attributes are defined here
+    id_anh: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    path_anh: {
+        type: DataTypes.STRING(1000),
+        allowNull: false
+    }
 }, {
-  freezeTableName: true
-  // Other model options go here
+    freezeTableName: true
+        // Other model options go here
 });
 
 const Binh_luan = sequelize.define('binh_luan', {
-  // Model attributes are defined here
-  id_binh_luan: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  id_phong_tro: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  id_nguoi_dung: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  noi_dung: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  hinh_anh: {
-    type: DataTypes.STRING(1000)
-  }
+    // Model attributes are defined here
+    id_binh_luan: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    id_phong_tro: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    id_nguoi_dung: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    noi_dung: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    hinh_anh: {
+        type: DataTypes.STRING(1000)
+    }
 }, {
-  freezeTableName: true
-  // Other model options go here
+    freezeTableName: true
+        // Other model options go here
 });
 
 const Bao_cao = sequelize.define('bao_cao', {
-  // Model attributes are defined here
-  id_bao_cao: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  id_nguoi_bao_cao: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  id_nguoi_bi_bao_cao: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  noi_dung: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  hinh_anh: {
-    type: DataTypes.STRING(1000)
-  }
+    // Model attributes are defined here
+    id_bao_cao: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    id_nguoi_bao_cao: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    id_nguoi_bi_bao_cao: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    noi_dung: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    hinh_anh: {
+        type: DataTypes.STRING(1000)
+    }
 }, {
-  freezeTableName: true
-  // Other model options go here
+    freezeTableName: true
+        // Other model options go here
 });
 
 const Quan_tri_vien = sequelize.define('quan_tri_vien', {
-  // Model attributes are defined here
-  id_quan_tri_vien: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  ten_quan_tri: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  mat_khau: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  sdt: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    validate: {
-      isEmail: {
-        msg: "Please enter a true email"
-      }
+    // Model attributes are defined here
+    id_quan_tri_vien: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    ten_quan_tri: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    mat_khau: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    sdt: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        validate: {
+            isEmail: {
+                msg: "Please enter a true email"
+            }
+        }
+    },
+    ngay_sinh: {
+        type: DataTypes.DATE,
+        allowNull: true
     }
-  },
-  ngay_sinh: {
-    type: DataTypes.DATE,
-    allowNull: true
-  }
 }, {
-  freezeTableName: true
-  // Other model options go here
+    freezeTableName: true
+        // Other model options go here
 });
 // (async function() {
 //   try {
@@ -380,57 +380,57 @@ const Quan_tri_vien = sequelize.define('quan_tri_vien', {
 //     console.error(error);
 //   }
 // })();
-Phong_tro.belongsTo(Nguoi_dung,{
-  foreignKey: {
-    name: "id_chu_so_huu",
-    allowNull: false
-  }
+Phong_tro.belongsTo(Nguoi_dung, {
+    foreignKey: {
+        name: "id_chu_so_huu",
+        allowNull: false
+    }
 });
-Nguoi_dung.hasMany(Phong_tro,{
-  foreignKey: {
-    name: "id_chu_so_huu",
-    allowNull: false
-  }
-});
-
-Tien_ich.belongsTo(Phong_tro,{
-  foreignKey: {
-    name: "id_phong_tro",
-    allowNull: false,
-    primaryKey: true
-  }
-});
-Phong_tro.hasOne(Tien_ich,{
-  foreignKey: {
-    name: "id_phong_tro",
-    allowNull: false,
-    primaryKey: true
-  }
+Nguoi_dung.hasMany(Phong_tro, {
+    foreignKey: {
+        name: "id_chu_so_huu",
+        allowNull: false
+    }
 });
 
-Hinh_anh.belongsTo(Phong_tro,{
-  foreignKey: {
-    name: "id_phong_tro",
-    allowNull: false,
-    primaryKey: true
-  }
+Tien_ich.belongsTo(Phong_tro, {
+    foreignKey: {
+        name: "id_phong_tro",
+        allowNull: false,
+        primaryKey: true
+    }
 });
-Phong_tro.hasMany(Hinh_anh,{
-  foreignKey: {
-    name: "id_phong_tro",
-    allowNull: false,
-    primaryKey: true
-  }
+Phong_tro.hasOne(Tien_ich, {
+    foreignKey: {
+        name: "id_phong_tro",
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+Hinh_anh.belongsTo(Phong_tro, {
+    foreignKey: {
+        name: "id_phong_tro",
+        allowNull: false,
+        primaryKey: true
+    }
+});
+Phong_tro.hasMany(Hinh_anh, {
+    foreignKey: {
+        name: "id_phong_tro",
+        allowNull: false,
+        primaryKey: true
+    }
 });
 
 module.exports = {
-  sequelizeInit,
-  Nguoi_dung,
-  Phong_tro,
-  Tien_ich,
-  Hinh_anh,
-  Binh_luan,
-  Bao_cao,
-  Quan_tri_vien
-}
-//sequelizeInit();
+        sequelizeInit,
+        Nguoi_dung,
+        Phong_tro,
+        Tien_ich,
+        Hinh_anh,
+        Binh_luan,
+        Bao_cao,
+        Quan_tri_vien
+    }
+    //sequelizeInit();
--- routers/authentication.js
+++ routers/authentication.js
@@ -8,113 +8,113 @@ const forgetPassword = require("../app/forgetPassword");
 require('dotenv').config();
 
 module.exports = (app, passport) => {
-  app.get("/login", function(req, res) {
-      res.render("user-login", {
-          message: req.flash("loginMessage")
-      });
-  });
+    app.get("/login", function(req, res) {
+        res.render("user-login", {
+            message: req.flash("loginMessage")
+        });
+    });
 
-  app.post("/login", passport.authenticate("local-login", {
-      successRedirect: '/', // redirect to the secure profile section
-      failureRedirect: '/login', // redirect back to the signup page if there is an error
-      failureFlash: true // allow flash messages
-  }), function(req, res) {
-      // res.redirect("/")
-      // if (req.body.remember) {
-      //   req.session.cookie.maxAge = 24 * 60 * 60 * 1000; // Cookie expires after 1 days
-      // } else {
-      //   req.session.cookie.expires = false; // Cookie expires at end of session
-      // }
-  });
+    app.post("/login", passport.authenticate("local-login", {
+        successRedirect: '/', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }), function(req, res) {
+        // res.redirect("/")
+        // if (req.body.remember) {
+        //   req.session.cookie.maxAge = 24 * 60 * 60 * 1000; // Cookie expires after 1 days
+        // } else {
+        //   req.session.cookie.expires = false; // Cookie expires at end of session
+        // }
+    });
 
-  app.get("/login/error", function(req, res) {
-      res.render("user-login", {
-          message: "Email already assigned"
-      });
-  });
+    app.get("/login/error", function(req, res) {
+        res.render("user-login", {
+            message: "Email không chính xác"
+        });
+    });
 
-  app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+    app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-  app.get("/auth/google/user-home", passport.authenticate("google", {
-          failureRedirect: '/login/error' ,
-          failureFlash: true
-      }),
-      function(req, res) {
-          // Successful authentication, redirect home.
-          res.redirect('/');
-      });
+    app.get("/auth/google/user-home", passport.authenticate("google", {
+            failureRedirect: '/login/error',
+            failureFlash: true
+        }),
+        function(req, res) {
+            // Successful authentication, redirect home.
+            res.redirect('/');
+        });
 
-  app.get('/auth/facebook',
-      passport.authenticate('facebook', { scope: ["email"] })
-  );
+    app.get('/auth/facebook',
+        passport.authenticate('facebook', { scope: ["email"] })
+    );
 
-  app.get('/auth/facebook/user-home',passport.authenticate('facebook', {
-        failureRedirect: '/login' ,
-        failureFlash: true
-      }),
-      function(req, res) {
-          // Successful authentication, redirect home.
-          res.redirect('/');
-      });
+    app.get('/auth/facebook/user-home', passport.authenticate('facebook', {
+            failureRedirect: '/login',
+            failureFlash: true
+        }),
+        function(req, res) {
+            // Successful authentication, redirect home.
+            res.redirect('/');
+        });
 
-  app.get("/signup", function(req, res) {
-      res.render("user-signup", {
-          message: req.flash("signupMessage")
-      })
-  });
+    app.get("/signup", function(req, res) {
+        res.render("user-signup", {
+            message: req.flash("signupMessage")
+        })
+    });
 
-  app.post("/signup", passport.authenticate("local-signup", {
-          successRedirect: '/', // redirect to the secure profile section
-          failureRedirect: '/signup', // redirect back to the signup page if there is an error
-          failureFlash: true // allow flash messages
-      }),function(req, res) {});
+    app.post("/signup", passport.authenticate("local-signup", {
+        successRedirect: '/', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }), function(req, res) {});
 
-  app.get("/logout", function(req, res) {
-      req.logout();
-      res.redirect("/");
-  });
+    app.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
+    });
 
-  app.get('/profile-change-password', function(req, res) {
-      if (req.isAuthenticated()) {
-          const user = req.session.passport.user;
-          console.log(user)
-          res.render('user-profile-change-password', { user: user });
-      } else { res.redirect('/login') }
-  });
+    app.get('/profile-change-password', function(req, res) {
+        if (req.isAuthenticated()) {
+            const user = req.session.passport.user;
+            console.log(user)
+            res.render('user-profile-change-password', { user: user });
+        } else { res.redirect('/login') }
+    });
 
-  app.post("/profile-change-password", function(req, res) {
-      const newInfo = req.body;
-      const oldInfo = req.session.passport.user;
+    app.post("/profile-change-password", function(req, res) {
+        const newInfo = req.body;
+        const oldInfo = req.session.passport.user;
 
-      const id_nguoi_dung = oldInfo.id_nguoi_dung;
-      const oldPass = oldInfo.mat_khau;
-      const typedOldPass = newInfo.oldPass;
-      const newPass = newInfo.newPass;
-      const renewPass = newInfo.reNewPass;
-      if (newPass != renewPass || !bcrypt.compareSync(typedOldPass, oldPass)) {
-          console.log("Sai mat khau");
-      } else {
-          changePassword(newInfo, oldInfo);
-          req.logout();
-          res.redirect("/login")
-      }
-  });
+        const id_nguoi_dung = oldInfo.id_nguoi_dung;
+        const oldPass = oldInfo.mat_khau;
+        const typedOldPass = newInfo.oldPass;
+        const newPass = newInfo.newPass;
+        const renewPass = newInfo.reNewPass;
+        if (newPass != renewPass || !bcrypt.compareSync(typedOldPass, oldPass)) {
+            console.log("Sai mat khau");
+        } else {
+            changePassword(newInfo, oldInfo);
+            req.logout();
+            res.redirect("/login")
+        }
+    });
 
-  app.get("/reset-password/:token", forgetPassword.checkToken);
+    app.get("/reset-password/:token", forgetPassword.checkToken);
 
-  app.post("/reset-password/:token", forgetPassword.resetPassword);
+    app.post("/reset-password/:token", forgetPassword.resetPassword);
 
-  app.get("/forget-password", function(req, res){
-    res.render("user-forget-password", { message: '' })
-  });
+    app.get("/forget-password", function(req, res) {
+        res.render("user-forget-password", { message: '' })
+    });
 
-  app.post("/forget-password", forgetPassword.forgetPassword)
+    app.post("/forget-password", forgetPassword.forgetPassword)
 
-  app.get("/verification", function(req, res) {
-      res.render("user-verification", { message: '' })
-  });
+    app.get("/verification", function(req, res) {
+        res.render("user-verification", { message: '' })
+    });
 
-  app.get("/verification-email", function(req, res) {
-      res.render("user-verification-email", { message: '' })
-  });
+    app.get("/verification-email", function(req, res) {
+        res.render("user-verification-email", { message: '' })
+    });
 }
