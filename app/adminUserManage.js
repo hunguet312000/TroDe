ce0987505bf46ddaf69f9b2a17baa5ed1477bfbe
theosiriscane@gmail.com <52742707+N-Linh@users.noreply.github.com>
2020-12-09 16:07:47 +0700

    add rating, user search, ...

--- app/adminUserManage.js
+++ app/adminUserManage.js
@@ -1,19 +1,29 @@
 const async = require('async')
 require('dotenv').config();
+const url = require("url")
 const { QueryTypes } = require('sequelize');
 const {sequelizeInit, Nguoi_dung, Phong_tro, Hinh_anh, Tien_ich, Binh_luan, Danh_sach_yeu_thich, sequelize} = require("../config/sequelize");
 const Op = require('Sequelize').Op
 
 exports.displayListUser = async(req, res) =>{
     if (req.isAuthenticated()) {
-
         try{
-            const queryValue = "SELECT *, count(id_phong_tro) as so_bai_dang  FROM nguoi_dung" +
-                                " left outer join phong_tro on phong_tro.id_chu_so_huu = nguoi_dung.id_nguoi_dung" +
-                                " group by nguoi_dung.id_nguoi_dung" +
-                                " order by nguoi_dung.id_nguoi_dung desc";
-            const nguoi_dung = await sequelize.query(queryValue, { type: QueryTypes.SELECT });
-            console.log(nguoi_dung)
+            let nguoi_dung;
+            if(JSON.stringify(req.query) == "{}"){
+                const queryValue = "SELECT *, count(id_phong_tro) as so_bai_dang  FROM nguoi_dung" +
+                                    " left outer join phong_tro on phong_tro.id_chu_so_huu = nguoi_dung.id_nguoi_dung" +
+                                    " group by nguoi_dung.id_nguoi_dung" +
+                                    " order by nguoi_dung.id_nguoi_dung desc";
+                nguoi_dung = await sequelize.query(queryValue, { type: QueryTypes.SELECT });
+            } else {
+                const searchUserValue = req.query.user;
+                nguoi_dung = await Nguoi_dung.findAll({
+                    where: {[Op.or] : [
+                        {ten_nguoi_dung : {[Op.like] : "%" + searchUserValue + "%"}}, 
+                        {ho_va_ten : {[Op.like] : "%" + searchUserValue + "%"}}
+                    ]}
+                })
+            }
             res.render("admin-control-user", { username: req.user.ten_nguoi_dung, userData : nguoi_dung });
         }catch(err) {
             console.log(err)
@@ -21,4 +31,11 @@ exports.displayListUser = async(req, res) =>{
     } else {
           res.redirect('/login');
     }
+}
+
+exports.searchUser = async(req, res) => {
+    res.redirect(url.format({
+        pathname: req.url,
+        query: req.body,
+    }))
 }
--- routers/admin.js
+++ routers/admin.js
@@ -22,5 +22,6 @@ module.exports = (app, passport) => {
   app.get("/admin-control-user",adminUserManage.displayListUser );
 
   app.get("/admin-control-report", adminReportManage.displayListReport);
+  app.post("/admin-control-user", adminUserManage.searchUser);
 
 }
