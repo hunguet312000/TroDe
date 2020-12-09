bdfbde276532d3b26c8ae46636b82c45749fd3fe
Miho1109 <minhhoan119@gmail.com>
2020-12-09 23:11:58 +0700

    MIHO admin paginate

--- app/adminReportManage.js
+++ app/adminReportManage.js
@@ -8,11 +8,15 @@ const sequelize = require("sequelize");
 const keywords_dict = require("../public/js/keywords_dict.js");
 const { sequelizeInit, Nguoi_dung, Phong_tro, Hinh_anh, Tien_ich, Binh_luan, Danh_sach_yeu_thich, Lich_hen, Bao_cao, Hinh_anh_bao_cao } = require("../config/sequelize");
 const Op = require('Sequelize').Op
+const paginate = require("./paginate");
 
 exports.displayListReport = async(req, res) => {
     if (req.isAuthenticated()) {
+      const calculatePagniate = await paginate.calculateAdminReportListPages(req, res);
         try{
             const bao_cao = await Bao_cao.findAll({
+              offset: calculatePagniate.offset,
+              limit: calculatePagniate.limit,
                 include: [{
                     model: Nguoi_dung,
                     required: true
@@ -25,7 +29,14 @@ exports.displayListReport = async(req, res) => {
             ],
             order: ['id_bao_cao']
             })
-            res.render("admin-control-report", { username: req.user.ten_nguoi_dung, userData : bao_cao });
+            res.render("admin-control-report", {
+              username: req.user.ten_nguoi_dung,
+              userData : bao_cao,
+              type: "/admin-control-report",
+              pages: calculatePagniate.pages,
+              current: req.params.page,
+              reportNum: calculatePagniate.reportNum
+            });
         }catch(err){
             console.log(err)
         }
