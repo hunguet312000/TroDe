008d83d67ff50d1b0b67dd579b240cc5a78858e7
Miho1109 <minhhoan119@gmail.com>
2020-12-13 21:22:57 +0700

    MIHO fix forget pass

--- app/forgetPassword.js
+++ app/forgetPassword.js
@@ -56,50 +56,6 @@ exports.forgetPassword = async (req, res) => {
     }catch(err){
         console.log(err)
     }
-    // console.log(req.body.email);
-    // dbConnection = mysql.createConnection(dbconfig);
-    // dbConnection.connect();
-    // dbConnection.query("SELECT * FROM nguoi_dung WHERE email = ?", [req.body.email], function(err, results){
-    //     console.log(results)
-    //     if (err){
-    //         throw err
-    //     }
-    //     else if(results.length == 0){
-    //         res.render("user-forget-password", { message: 'Tài khoản không hợp lệ' })
-    //     }
-    //     else {
-    //         let token = jwt.sign({_id: results[0].id_nguoi_dung}, process.env.RESET_PASSWORD_KEY, {expiresIn: '10m'});
-    //         dbConnection.query("UPDATE nguoi_dung SET token = ? WHERE id_nguoi_dung = ?",[token, results[0].id_nguoi_dung], function(err, rows){
-    //             if(err){
-    //                 throw err
-    //             }
-    //         })
-    //         var transporter = nodemailer.createTransport({
-    //             service: 'gmail',
-    //             auth: {
-    //               user: 'vuthiphuong2910@gmail.com',
-    //               pass: 'ghiaog123'
-    //             }
-    //           });
-
-    //           var mailOptions = {
-    //             from: 'vuthiphuong2910@gmail.com',
-    //             to: req.body.email,
-    //             subject: 'Sending Email using Node.js',
-    //             html: '<p>Click <a href="http://localhost:3000/reset-password/' + token + '">here</a> </p>'
-    //           };
-
-    //           transporter.sendMail(mailOptions, function(error, info){
-    //             if (error) {
-    //               console.log(error);
-    //             } else {
-    //               console.log('Email sent: ' + info.response);
-    //             }
-    //           });
-    //         res.render("user-verification");
-    //     }
-    // });
-
 }
 
 exports.checkToken = async (req,res) => {
@@ -123,23 +79,14 @@ exports.checkToken = async (req,res) => {
            }catch(err){
                res.send("Sai cú pháp")
            }
-        //    dbConnection = mysql.createConnection(dbconfig);
-        //    dbConnection.connect();
-        //    dbConnection.query("SELECT token FROM nguoi_dung where token = ?", [reset_link], function(error, result){
-        //         if( result.length == 0 ) {
-        //             res.send("token does not exist")
-        //         }else{
-        //             res.render('user-reset-password', {message:'', action:"/reset-password/" + reset_link})
-        //         }
-        //    });
-
        })
    }
 }
 
 exports.resetPassword = async(req, res) => {
+    console.log(req.body);
     var reset_link = req.params.token;
-    if ( req.body.password == req.body.passwordcf ){
+    if ( req.body.password == req.body.rePassword ){
         console.log(req.body);
         const hashPass = bcrypt.hashSync(req.body.password, 10);
         try{
@@ -153,15 +100,6 @@ exports.resetPassword = async(req, res) => {
         }catch(err){
             res.render('user-reset-password', {message:'Thay đổi mật khẩu không thành công. Xin vui lòng thử lại.', action:"/reset-password/" + reset_link})
         }
-        // dbConnection = mysql.createConnection(dbconfig);
-        // dbConnection.connect();
-        // dbConnection.query("UPDATE nguoi_dung SET mat_khau = ? WHERE token = ?",[hashPass, reset_link], function(err, rows){
-        //     if(err){
-        //         throw err
-        //     }else{
-        //         console.log('success');
-        //     }
-        // })
         res.redirect('/login');
     }
     else{
